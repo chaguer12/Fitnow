7723af36fd991bad7c81b04bb9ba991a90be55a9
zoolotoui <chag.med.amine@gmail.com>
2024-03-25 23:21:21 +0000

    core:working on migrations

--- database/migrations/2024_03_25_225404_create_progress_table.php
+++ database/migrations/2024_03_25_225404_create_progress_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('progress', function (Blueprint $table) {
             $table->id();
+            $table->string('poids');
+            $table->string('taille');
+            $table->string('age');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
